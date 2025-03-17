3f88635cc482f017518f56e8f56754998924b420
Alexan2a <sashaavdeyuk@gmail.com>
2025-03-17 13:16:23 +0300

    rm check (mb need new), add valid

--- Polyphase_filter/FIR_lowpass_decimator_v2/tb.v
+++ Polyphase_filter/FIR_lowpass_decimator_v2/tb.v
@@ -3,9 +3,8 @@
 
 module tb();
 
-  reg clk, clk_fs_new, clk_fs_old, c_we, nrst, check_en;
+  reg clk, clk_fs_new, clk_fs_old, c_we, nrst, en, valid;
   wire clk_fs_new_div;
-  reg [15:0] check;
   wire [15:0] out;
   
   reg [15:0] in_vect[0:1024], in;
@@ -21,18 +20,20 @@ module tb();
   `COEFFS_ARRAY_0
 
   initial begin
-  check_en = 0;
-  N = 0;
-  K = 0;
-  err_cnt = 0;
-  in = 0;
-  clk = 0;
-  clk_fs_new = 0;
-  clk_fs_old = 0;
-  c_we = 1;
-  nrst = 0;
-  #5 nrst = 1;
-  #130000 check_en = 1;
+    N = 0;
+    K = 0;
+    en = 1;
+    valid = 1;
+    err_cnt = 0;
+    in = 0;
+    clk = 0;
+    clk_fs_new = 0;
+    clk_fs_old = 0;
+    c_we = 1;
+    nrst = 0;
+    #5 nrst = 1;
+    #500000 valid = 0;
+    #11 valid = 1;
   end
 
   clock_divider #(D) i_clk_div_0(
@@ -49,8 +50,8 @@ module tb();
   always @(posedge clk or negedge nrst) begin
     if (!nrst) begin
       cnt <= 0;
-    end else if (cnt == 255) begin
-      cnt <= 255;
+    end else if (cnt == 127) begin
+      cnt <= 127;
       c_we <= 1'b0;
     end else begin
       cnt <= cnt+1;
@@ -82,17 +83,6 @@ module tb();
       $fdisplay(FILE_1, "0b%bs16", out);
     end
 
-  always @(posedge clk_fs_old) begin
-    if (check_en) begin
-      check = ((($signed(-coeffs_array_0[K]))>>> 2) + 1) >>> 1;
-      if (out != check) begin
-	err_cnt = err_cnt + 1;
-        $error("out: expected = %h, real = %h",check, out);
-      end
-      K = K + 1;
-    end
-  end
-
   initial begin
     #1000000 $display("Output errors: %d", err_cnt);
     $stop();
@@ -101,6 +91,8 @@ module tb();
   fir_decimator #(ORD,M,D,COEFF_SIZE,SAMPLE_SIZE) i_fir(
     .nrst(nrst),
     .clk(clk),
+    .valid_in(valid),
+    .valid_out(),
     .din(in),
     .dout(out),
 
