8a3452f9abc68abafb5e679b186c0ae286ebb222
Alexan2a <sashaavdeyuk@gmail.com>
2025-06-12 14:48:25 +0300

    Fix TestBench + test_examples

--- Adaptive_filtering/LMS_filter/TB.v
+++ Adaptive_filtering/LMS_filter/TB.v
@@ -1,10 +1,11 @@
 module tb();
 
-  reg [15:0] in_vect[0:1920], in;
+  reg [15:0] u_vect[0:200000];
+  reg [15:0] d_vect[0:200000];
 
   wire[15:0] out;
   reg clk, clk_fs_0, clk_fs_1, nrst, mode, mu_we;
-  reg valid_d, valid_u;
+  reg valid;
   reg [15:0] u, d;
   wire valid_out;
   wire [15:0] mu;
@@ -20,34 +21,38 @@ module tb();
   integer FILE_1;
 
   initial
-    begin: read_block
-      $readmemb("in_Data.txt", in_vect);
+    begin: read_block_u
+      $readmemb("In_noise.txt", u_vect);
+    end
+
+  initial
+    begin: read_block_d
+      $readmemb("In_data.txt", d_vect);
     end
 
   initial
     begin: file_IO_block
-      FILE_1 = $fopen("Data_out.txt", "w");
-      #1000900 $fclose(FILE_1);
+      FILE_1 = $fopen("Out_data.txt", "w");
+      #4258250000 $fclose(FILE_1);
     end
 
   initial begin
-
      clk = 1'b1;
      clk_fs_0 = 1'b1;
      clk_fs_1 = 1'b1;
+     N = -1;
      mode = 0;
      nrst = 1'b1;
-     valid_d = 1;
-     valid_u = 1;
-     u = 16'b0000100000000001;
-     d = 16'b1100000000000000;
+     valid = 1;
+     u = 0;
+     d = 0;
      mu_we = 1'b1;
   #5 nrst = 1'b0;
   #5 nrst = 1'b1;
   #5 mu_we = 1'b0;
-  #65000 valid_u = 0;
-  #5000 valid_u = 1;
-  #1000000 mode = 1;
+  #65000 valid = 0;
+  #5000 valid = 1;
+  #1133750000 mode = 1;
   end
 
 
@@ -59,20 +64,21 @@ module tb();
 
   always @(clk_fs) begin
       N = N + 1;
-      in = in_vect[N];
-      u = u+1;
+      u = u_vect[N];
+      d = d_vect[N];
   end
 
   always @(clk_fs)
     begin: write_block
-      $fdisplay(FILE_1, "0b%bs16", out);
+      if (valid_out) begin
+        $fdisplay(FILE_1, "0b%bs16", out);
+      end
     end
 
   LMS_filter #(S, C) i_fir(
     .nrst(nrst),
     .clk(clk),
-    .valid_d_in(valid_d),
-    .valid_u_in(valid_u),
+    .valid_in(valid),
     .valid_out(valid_out),
     .mode(mode),
     .mu_in(mu),
